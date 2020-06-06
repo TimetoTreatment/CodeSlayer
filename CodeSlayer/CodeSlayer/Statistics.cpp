75decd02ed55000a91822d9384c1b6e2027e62d1
dh5252 <52120998+dh5252@users.noreply.github.com>
2020-06-06 18:54:46 +0900

    1.11
    Add Statisitcs Class

--- CodeSlayer/CodeSlayer/Statistics.cpp
+++ CodeSlayer/CodeSlayer/Statistics.cpp
@@ -1,23 +1,263 @@
 #include "Statistics.h"
 
 
-
-
-void Statistics::Main()
-{
-
+Statistics::Statistics() {
+	mConsole = Console::Instance();
+	mKeyboard = Keyboard::Instance();
 }
 
 
 
 
+void Statistics::LoadStatistics() {
+
+	fstream file;
+
+	// 정확도 저장
+
+	file.open("Assets/statistics/wordaccuracy.txt");
+
+
+	int tmp;
+	
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mWordAccuracy.push_back(tmp);
+		}
+	}
+
+	file.close();
+
+	file.open("Assets/statistics/shortaccuracy.txt");
+
+
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mShortAccuracy.push_back(tmp);
+		}
+	}
+	file.close();
+
+	file.open("Assets/statistics/longaccuracy.txt");
+
+	
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mLongAccuracy.push_back(tmp);
+		}
+	}
+	file.close();
+
+	
+	// speed 저장
+
+	file.open("Assets/statistics/wordspeed.txt");
+
+
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mWordSpeed.push_back(tmp);
+		}
+	}
+
+	file.close();
+
+	file.open("Assets/statistics/shortspeed.txt");
+
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mShortSpeed.push_back(tmp);
+		}
+	}
+
+	file.close();
+
+	file.open("Assets/statistics/longspeed.txt");
+
+	
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			file >> tmp;
+
+			mLongSpeed.push_back(tmp);
+		}
+	}
+
+	file.close();
+
+}
+
+int Statistics::ReturnAverage(const string& texttype,const string& object ) {
+
+	int sum = 0,tmplen;
+
+	if (object == "speed") {
+		if (texttype == "word") {
+			tmplen = mWordSpeed.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mWordSpeed.at(i);
+			}
+			sum /= mWordSpeed.size(); // avg 구하기 위해 개수로 나누었음
+		}
+		else if (texttype == "short") {
+			tmplen = mShortSpeed.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mShortSpeed.at(i);
+			}
+			sum /= mShortSpeed.size();
+		}
+		else if (texttype == "long") {
+			tmplen = mLongSpeed.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mLongSpeed.at(i);
+			}
+			sum /= mLongSpeed.size();
+		}
+
+	}
+	else if (object == "accuracy") {
+		if (texttype == "word") {
+			tmplen = mWordAccuracy.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mWordAccuracy.at(i);
+			}
+			sum /= mWordAccuracy.size();
+		}
+		else if (texttype == "short") {
+			tmplen = mShortAccuracy.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mShortAccuracy.at(i);
+			}
+			sum /= mShortSpeed.size();
+		}
+		else if (texttype == "long") {
+			tmplen = mLongAccuracy.size();
+			for (int i = 0;tmplen > i;i++) {
+				sum += mLongAccuracy.at(i);
+			}
+			sum /= mLongSpeed.size();
+		}
+	}
+
+	return sum;
+}
 
 
 
+void Statistics::RenderStatistics()
+{
+	mConsole->Draw("Assets/layout/statistics_main.txt", "white", 0, 1);
+	mConsole->Draw("Assets/layout/statistics_recordtable.txt", "white", mLeftRecordTableStartX, mLeftRecordTableStartY);
+	mConsole->Draw("Assets/layout/statistics_recordtable.txt", "white", mRightRecordTableStartX, mRightRecordTableStartY);
+
+
+	mConsole->Draw("Assets/layout/statistics_averagetable.txt", "white", mLeftAverageTableStartX, mLeftAverageTableStartY);
+	mConsole->Draw("Assets/layout/statistics_averagetable.txt", "white", mRightAverageTableStartX, mRightAverageTableStartY);
+
+	int wordSpeedAverage = 0;
+	int shortSpeedAverage = 0;
+	int longSpeedAverage = 0;
+
+	int tmplen = 0;
+
+
+	/*1~5 출력*/
+	/*leftTable 타수 출력*/
+
+	tmplen = mLongSpeed.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mLeftRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mLeftRecordTableStartY + mRecordTableIntervalY);
+		cout << mLongSpeed[i];
+	}
+
+	tmplen = mShortSpeed.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mLeftRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mLeftRecordTableStartY + mRecordTableIntervalY * 2);
+		cout << mShortSpeed[i];
+	}
+
+	tmplen = mWordSpeed.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mLeftRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mLeftRecordTableStartY + mRecordTableIntervalY * 3);
+		cout << mWordSpeed[i];
+	}
+
+	/*rightTable 정확도 출력*/
+
+	tmplen = mLongAccuracy.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mRightRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mRightRecordTableStartY + mRecordTableIntervalY);
+		cout << mLongAccuracy[i];
+	}
+
+	tmplen = mShortAccuracy.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mRightRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mRightRecordTableStartY + mRecordTableIntervalY * 2);
+		cout << mShortAccuracy[i];
+	}
+
+	tmplen = mWordAccuracy.size();
+	for (int i = 0;tmplen > i;i++) {
+		mConsole->CursorPosition(mRightRecordTableStartX + mRecordTableStartToInputIntervalX + mRecordTableIntervalX * i, mRightRecordTableStartY + mRecordTableIntervalY * 3);
+		cout << mWordAccuracy[i];
+	}
+
+	/*각 평균 출력*/
+	/*left 타수*/
+	mConsole->CursorPosition(mLeftAverageTableStartX + mAverageTableStartToInputIntervalX , mLeftAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("long", "speed");
+
+	mConsole->CursorPosition(mLeftAverageTableStartX + mAverageTableStartToInputIntervalX + mAverageTableIntervalX, mLeftAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("short", "speed");
+
+	mConsole->CursorPosition(mLeftAverageTableStartX + mAverageTableStartToInputIntervalX + mAverageTableIntervalX * 2, mLeftAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("word", "speed");
+	
+
+	/*right 정확도*/
+	mConsole->CursorPosition(mRightAverageTableStartX + mAverageTableStartToInputIntervalX, mRightAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("long", "accuracy");
+
+	mConsole->CursorPosition(mRightAverageTableStartX + mAverageTableStartToInputIntervalX + mAverageTableIntervalX, mRightAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("short", "accuracy");
+
+	mConsole->CursorPosition(mRightAverageTableStartX + mAverageTableStartToInputIntervalX + mAverageTableIntervalX * 2, mRightAverageTableStartY + mAverageTableIntervalY);
+	cout << ReturnAverage("word", "accuracy");
 
 
 
+	/*Test 위해 임시로 넣어놓음*/
+	mConsole->CursorPosition(80, 40);
 
+}
+
+
+
+void Statistics::Main()
+{
+
+	LoadStatistics();
+	RenderStatistics();
+
+	/*mConsole->Clear();*/
+	
+}
 
 Statistics* Statistics::sInstance = nullptr;
 
--- CodeSlayer/CodeSlayer/Statistics.h
+++ CodeSlayer/CodeSlayer/Statistics.h
@@ -1,14 +1,73 @@
 #pragma once
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <fstream>
+#include "Console.h"
+#include "Keyboard.h"
+
+using namespace std;
+
 class Statistics
 {
+
 private:
 
+	Console *mConsole;
+	Keyboard *mKeyboard;
 
+	vector<int> mLongAccuracy;
+	vector<int> mLongSpeed;
+
+	vector<int> mWordAccuracy;
+	vector<int> mWordSpeed;
+
+	vector<int> mShortAccuracy;
+	vector<int> mShortSpeed;
+
+	int mAverageSpeed;
+	int mAverageAccuarcy;
+
+	const int mLeftRecordTableStartX = 9;
+	const int mLeftRecordTableStartY = 17;
+	const int mRightRecordTableStartX = 60;
+	const int mRightRecordTableStartY = 17;
+
+	const int mLeftAverageTableStartX = 13;
+	const int mLeftAverageTableStartY = 26;
+	const int mRightAverageTableStartX = 64;
+	const int mRightAverageTableStartY = 26;
+
+	const int mRecordTableStartToInputIntervalX = 11;
+	const int mAverageTableStartToInputIntervalX = 12;
+
+	const int mRecordTableIntervalX = 6;
+	const int mRecordTableIntervalY = 2;
+
+	const int mAverageTableIntervalX = 8;
+	const int mAverageTableIntervalY = 2;
 
 public:
 
 	/* 메인 함수 */
 	void Main();
+
+	/* 파일 불러오기 함수 */
+	void LoadStatistics();
+
+	/* 평균 구하기 함수 */
+	int ReturnAverage(const string& texttype,const string& object);
+
+	/* 데이터 출력 함수*/
+
+
+	/* 콘솔창 */
+	void RenderStatistics();
+
+
+	/* 생성자, 소멸자 */
+	Statistics();
+	
 
 
 private:
